ab499f8551798063b1cab9e14806ed2e70455051
Akbar Sami <mdsami6251@gmail.com>
2024-04-14 12:22:05 +0600

    leetcode

---
+++ PowerOfTwo.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        if n<=0:
+            return False
+        return (n & (n-1))==0
+
+obj=Solution()
+print(obj.isPowerOfTwo(18))
+        
+        
